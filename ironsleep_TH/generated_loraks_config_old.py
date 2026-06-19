4f325fc69b9f1cdb77106197ab0b0b7d2ec7a7b9
Friedrich Krohn <>
2026-06-19 13:23:47 +0200

    added automated configuration

---
+++ ironsleep_TH/generated_loraks_config_old.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+# Auto-generated LORAKS config.
+# Inspect this file before running reconstruction.
+
+sub_ses = [['40108.fb', ['20260417']],
+ ['40135.5a', ['20260421']],
+ ['42298.19', ['20260430']],
+ ['42754.84', ['20260424']],
+ ['43469.55', ['20260421']],
+ ['43516.49', ['20260428']],
+ ['43798.f3', ['20260518']]]
+
+input_parent = '/data/pt_03187/data/in_vivo/source'
+output_parent = '/data/pt_03187/data/in_vivo/source'
+name_storage_dir = 'nii_loraks_recon'
+
+with_smaps = False
+smaps_per_session = 0
+
+pdw_raw = [['meas_MID00085_pdw_kp_mtflash3d_v1t3_0p6_20260417-130737.dat'],
+ ['meas_MID00075_pdw_kp_mtflash3d_v1t3_0p6_20260421-181218.dat'],
+ ['meas_MID00189_pdw_kp_mtflash3d_v1t3_0p6_20260430-154030.dat'],
+ ['meas_MID00046_pdw_kp_mtflash3d_v1t3_0p6_20260424-170329.dat'],
+ ['meas_MID00028_pdw_kp_mtflash3d_v1t3_0p6_20260421-155906.dat'],
+ ['meas_MID00229_pdw_kp_mtflash3d_v1t3_0p6_20260428-174721.dat'],
+ ['meas_MID00203_pdw_kp_mtflash3d_v1t3_0p6_20260518-172606.dat']]
+
+t1w_raw = [['meas_MID00077_t1w_kp_mtflash3d_v1t3_0p6_20260417-122248.dat'],
+ ['meas_MID00068_t1w_kp_mtflash3d_v1t3_0p6_20260421-174010.dat'],
+ ['meas_MID00187_t1w_kp_mtflash3d_v1t3_0p6_20260430-152936.dat'],
+ ['meas_MID00044_t1w_kp_mtflash3d_v1t3_0p6_20260424-165238.dat'],
+ ['meas_MID00026_t1w_kp_mtflash3d_v1t3_0p6_20260421-154816.dat'],
+ ['meas_MID00227_t1w_kp_mtflash3d_v1t3_0p6_20260428-173620.dat'],
+ ['meas_MID00197_t1w_kp_mtflash3d_v1t3_0p6_20260518-165747.dat']]
+
+mtw_raw = [['meas_MID00081_mtw_kp_mtflash3d_v1t3_0p6_20260417-124236.dat'],
+ ['meas_MID00072_mtw_kp_mtflash3d_v1t3_0p6_20260421-175957.dat'],
+ ['meas_MID00192_mtw_kp_mtflash3d_v1t3_0p6_20260430-154927.dat'],
+ ['meas_MID00048_mtw_kp_mtflash3d_v1t3_0p6_20260424-171231.dat'],
+ ['meas_MID00030_mtw_kp_mtflash3d_v1t3_0p6_20260421-160801.dat'],
+ ['meas_MID00231_mtw_kp_mtflash3d_v1t3_0p6_20260428-175620.dat'],
+ ['meas_MID00201_mtw_kp_mtflash3d_v1t3_0p6_20260518-171739.dat']]
+
+ernst_raw = [['meas_MID00084_ernst_kp_mtflash3d_v1s_0p5_sag_20260417-125403.dat'],
+ ['meas_MID00082_ernst_kp_mtflash3d_v1s_0p5_sag_20260421-184332.dat'],
+ ['meas_MID00195_ernst_kp_mtflash3d_v1s_0p5_sag_20260430-160131.dat'],
+ ['meas_MID00051_ernst_kp_mtflash3d_v1s_0p5_sag_20260424-172322.dat'],
+ ['meas_MID00033_ernst_kp_mtflash3d_v1s_0p5_sag_20260421-161922.dat'],
+ ['meas_MID00234_ernst_kp_mtflash3d_v1s_0p5_sag_20260428-180707.dat'],
+ ['meas_MID00206_ernst_kp_mtflash3d_v1s_0p5_sag_20260518-173757.dat']]
+
+b1afi_ptx_raw = None
+b1afi_stx_raw = None
